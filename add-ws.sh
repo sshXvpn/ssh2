e6ea54bf24cff034d872a57af3649ceb06a99677
sshXvpn <133983813+sshXvpn@users.noreply.github.com>
2023-05-19 10:08:20 +0800

    Update add-ws.sh

--- add-ws.sh
+++ add-ws.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+export TERM=xterm
+export domain=$(cat /etc/xray/domain)
+MYIP=$(wget -qO- icanhazip.com)
+clear
+read -p "   Username: " Login
 
+read -p "   Expired (days): " masaaktif
+uuid=$(cat /proc/sys/kernel/random/uuid)
+
+echo -e "$Login"
+echo -e "$exp"
+echo -e "$https" #port 443
+
+echo -e "$http" #port 80
+
+# incomplet code
